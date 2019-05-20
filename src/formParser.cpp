a2c4c8bb99ef74af32197fd585c1ddc2eba2acc5
ColdIV <yufoolme@gmail.com>
2019-05-20 19:09:43 +0200

    fixed errors in formParser & added explanation

--- src/formParser.cpp
+++ src/formParser.cpp
@@ -1,36 +1,60 @@
 #include <iostream>
+#include <string.h>
 
-char* getValue(char *str, char *key) {
-     int end = 0;
-     str++;
-     while(*str && *str != *key) str++;
-     if (*str == *key) {
-        while(*key && *str && *(str) == *(key)) {
-            std::cout << "Str: " << *str << " Key: " << *key << std::endl;
-            str++;
-            key++;
-            
-            if (!*key) {
-                // It works!
-                // Now get the next value
-                str++;
-                for (end = 0; *str && str[end] != '&'; end++);
-                str[end] = '\0';
-                return str;
-            }
-        }
-        return NULL;
-     }
+/*
+	This functions extracts the value of a specific key
+	char *str		takes a string
+	char *key		takes a string
+	returns 	a string or NULL if key could not be found
+	
+	Example:
+		getValueOfKey("?name=Admin&password=abc123", "name")
+		// -> returns "Admin"
+		getValueOfKey("?name=Admin&password=abc123", "password")
+		// -> returns "abc123"
+		
+	Note: Those are NOT secure credentials!
+*/
+char* getValueOfKey(char *str, char *key) {
+	int end = 0;
+	str++;
+	while(*str && *str != *key) str++;
      
-     return NULL;
+	char *tmpKey = key;
+     
+	while(*tmpKey && *str && *str == *tmpKey) {
+		str++;
+		tmpKey++;
+		
+		if (!*tmpKey && *str == '=') {
+			str++;
+			for (end = 0; *(str + end) && str[end] != '&'; end++);
+			str[end] = '\0';
+			return str;
+		}
+		
+		if ((!*tmpKey && *str != '=') || (*str != *tmpKey)) {
+			tmpKey = key;
+			while(*str && *str != *key) str++;
+		}
+	}
+       
+	return NULL;
 }
 
 
 int main() {
-    char str[] = "?name=test&password=test2";
+	char str[] = "?name3=test&name5=name&password=test2&name=test3&name2=test4";
+	char *tmpStr = new char[strlen(str)];
     
-    std::cout << "Result: " << getValue(str, "name") << std::endl;
+	tmpStr = getValueOfKey(str, "name5");
+	
+	if (tmpStr == NULL) {
+		std::cout << "Error! Key not found." << std::endl;
+	} else {
+		std::cout << "Result: " << tmpStr << std::endl;
+	}
     
-    system("pause");
-    return 0;
+	system("pause");
+	return 0;
 }
