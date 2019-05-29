2d4ccfb0f1e16e5bbd85207dc10b75b222d3f6be
ColdIV <yufoolme@gmail.com>
2019-05-29 21:17:25 +0200

    fixed formFunctions (did stupid stuff there)

--- src/formFunctions.cpp
+++ src/formFunctions.cpp
@@ -1,7 +1,27 @@
 #define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
+#include <time.h>
+#include <string.h>
+#include "formFunctions.h"
 
-char* getValueOfKey(const char *str, char *key, char separator = '&') {
+/*
+	This functions extracts the value of a specific key
+	const char *str     takes a string
+	char *key		    takes a string
+	char separator      takes a char or nothing (default is '&')
+	returns             a string or NULL if key could not be found
+
+	Example:
+	getValueOfKey("?name=Admin&password=abc123", "name")
+	// -> returns "Admin"
+	getValueOfKey("?name=Admin&password=abc123", "password")
+	// -> returns "abc123"
+	getValueOfKey("name=test; password=test_pass;", "name", ';')
+	// -> returns "test"
+	
+	Note: Those are NOT secure credentials!
+*/
+char* getValueOfKey(const char *str, char *key, char separator) {
 	int end = 0;
 	char *tmpStr;
 	char *tmpKey = key;
@@ -40,12 +60,45 @@ char* getValueOfKey(const char *str, char *key, char separator = '&') {
 	return NULL;
 }
 
+/*
+	This functions returns an timestamp from a date
+	char *date          takes a date as "yyyy-mm-dd"
+	returns             date as timestamp (comparable with >, <, ==, ...)
+*/
+time_t getTimestamp(char *date) {
+	tm tmdate = {0};
+	tmdate.tm_year = atoi(&date[0]) - 1900;
+	tmdate.tm_mon = atoi(&date[5]) - 1;
+	tmdate.tm_mday = atoi(&date[8]);
+	time_t timestamp = mktime(&tmdate);
 
-int main() {
-	char *str = "vorname=hans&nachname=peter&name=hanspeter";
-	char *key = "name";
-	std::cout << getValueOfKey(str, key) << std::endl;
+	return timestamp;
+}
 
-	system("pause");
+/*
+	This is the compare function for qsort
+	
+	Usage:
+		Pass the following as last parameter for qsort:
+		int(*compare)(const void *, const void*)
+*/
+int compare(const void *p1, const void *p2) {
+	time_t *pa = (time_t*)p1;
+	time_t *pb = (time_t*)p2;
+
+	if (*pa < *pb) return -1;
+	if (*pa > *pb) return 1;
 	return 0;
+}
+
+
+/*
+	This functions sets a cookie
+	char *key           takes a string as key
+	char *value         takes a string as value
+	
+	ATTENTION: Cookies have to be set before any other output!
+*/
+void setCookie(char *key, char *value) {
+	std::cout << "Set-Cookie:" << key << " = " << value << ";\r\n";
 }
