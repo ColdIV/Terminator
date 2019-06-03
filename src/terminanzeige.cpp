0300ed350a0bd5afccc0ab94dd52fcc5d141107a
meh3000 <50619849+meh3000@users.noreply.github.com>
2019-06-03 11:20:43 +0200

    terminanzeige in bad und unfertig

---
+++ src/terminanzeige.cpp
@@ -0,0 +1,21 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include "fileFunctions.h"
+#include "formFunctions.h"
+using namespace std;
+
+int main(){
+
+	cout << " Content-type: text/html\n\n";
+	cout << "<tr>";
+	cout << "<td>19.05.2019 14.00 Uhr< / td>";
+	cout << "<td>Meeting C++< / td>";
+	cout << "<td><button><i class = 'fa fa - pencil'>ändern< / i>< / button><button><i class = 'fa fa - trash'>löschen< / i>< / button>< / td>";
+	cout << "< / tr>";
+
+	return 0;
+}
+void 
