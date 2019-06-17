ced9212d186e4a4033e78c4f866aca88c8db7ef4
meh3000 <50619849+meh3000@users.noreply.github.com>
2019-06-17 22:11:57 +0200

    update login.cpp

--- src/login.cpp
+++ src/login.cpp
@@ -14,35 +14,35 @@ struct Account {
     int id;
 };
 
-int login() {   
+int login(char* user, char* password) {
 	int i =0;
     Account account;
 	char *reghtml = getenv("Key");
-	char *data = "name=wassi&password=1234";
-	
-	account.user = getValueOfKey(data, "name");
-	account.password = getValueOfKey(data, "password");
+	char *data = (char*)"name=wassi&password=1234";
+
+	account.user = getValueOfKey(data, (char*)"name");
+	account.password = getValueOfKey(data, (char*)"password");
 
 	if (validateLogin(account.user, account.password)){
-		setCookie("name", account.user);
-		setCookie("password", account.password);
+		setCookie((char*)"name", account.user);
+		setCookie((char*)"password", account.password);
 		// Ausgabe des Menüs
-		std::cout << getTemplate("../htdocs/menue.html");
+		std::cout << getTemplate((char*)"../htdocs/menue.html");
 	}
-	else if(*reghtml == "../htdocs/reg.html"){
+	else if((char*)*reghtml == (char*)"../htdocs/reg.html"){
 		Account *accounts;
 		size_t size = 0;
-		accounts = (Account*) writeStructs("accounts.bin",&data, &size, sizeof(Account));
-	
+		writeStructs("accounts.bin",&data, size, sizeof(Account));
+
 			for (int i = 0; i < size; i++){
-				if (accounts[i].user == user && accounts[i].password){
+				if (accounts[i].user == user && accounts[i].password == password){
 				return true;
 				}
-			}	
+			}
 	}
 	else {
 		// Fehlermeldung, Login falsch
-		std::cout << getTemplate("../htdocs/login.html");
+		std::cout << getTemplate((char*)"../htdocs/login.html");
 	}
 
     return 0;
@@ -50,11 +50,11 @@ int login() {
 
 bool validateLogin(char *user, char *password){
 	Account *accounts;
-	size_t size = 0;
-	accounts = (Account*) readStructs("accounts.bin", &size, sizeof(Account));
-	
+	size_t *size = 0;
+	accounts = (Account*) readStructs("accounts.bin", size, sizeof(Account));
+
 	for (int i = 0; i < size; i++){
-		if (accounts[i].user == user && accounts[i].password){
+		if (accounts[i].user == user && accounts[i].password == password){
 			return true;
 		}
 	}
