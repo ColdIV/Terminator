896d1866803b5cca520d99fca03db7e3c3ce4ab7
meh3000 <50619849+meh3000@users.noreply.github.com>
2019-06-26 17:47:59 +0200

    Update terminanzeige.cpp

--- src/terminanzeige.cpp
+++ src/terminanzeige.cpp
@@ -1,4 +1,3 @@
-#define _CRT_SECURE_NO_WARNINGS
 #include <iostream>
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,34 +12,46 @@ struct Appointment {
 	char *time[100];
 	char *description;
 	int id;
-	int userid;
-};// feste größe in formFunction.h hab da die strukts abgelegt lol
+	int Appointmentid;
+};
 
-void appointmentHTML(){
+void appointmentHTML(Appointment, int id){
 	Appointment * test1 = 0;
 	size_t num = 0;
 	test1 = (Appointment*)readStructs("termin.bin", &num, sizeof(Appointment));
 
-	for (int i = 0; i<= num ;i++){
-	cout << "Content-type:text/html\r\n\r\n";
-	cout << "<tr>";
-	// Error weil beim ausgeben- > operator wird nicht erkannt
-	//cout << "<td>" << dateTime() << "< / td>";
-	//cout << "<td>" << descriptionF() << ";< / td>";
-	cout << "<td><button><i class = 'fa fa - pencil'>ändern< / i>< / button><button><i class = 'fa fa - trash'>löschen< / i>< / button>< / td>";
-	cout << "< / tr>";
-}
-}
+    char *cookieData = getenv("HTTP_COOKIE");
+    char *userID;
+    userID = getValueOfKey(cookieData, (char*)"id", ';');
 
+    if(userID != id){
+        cout << "Der User hat keine oder eine falsche id";
+    }
+    else{
+      for (int i = 0; i<= num ;i++){
+	  cout << "Content-type:text/html\r\n\r\n";
+	  cout << "<tr>";
+	  // Error weil beim ausgeben- > operator wird nicht erkannt
+	  //cout << "<td>" << dateTime() << "< / td>";
+	  //cout << "<td>" << descriptionF() << ";< / td>";
+	  cout << "<td><button><i class = 'fa fa - pencil'>ändern< / i>< / button><button><i class = 'fa fa - trash'>löschen< / i>< / button>< / td>";
+      cout << "< / tr>";
+      }
+    }
+}
+/*
 void dateTime(){
-	/*
-	This functions returns an timestamp from a date
-	char *date          takes a date as "yyyy-mm-dd"
-	returns             date as timestamp (comparable with >, <, ==, ...)
-	*/
-	time_t getTimestamp(char *date);
-}
 
+	//This functions returns an timestamp from a date
+	//char *date          takes a date as "yyyy-mm-dd"
+	//returns             date as timestamp (comparable with >, <, ==, ...)
+
+	time_t getTimestamp(char *date);
+}*/
 void descriptionF(){
 	cout << "Test"; //
 }
+
+//appointmentDelete
+//appointmentAdd
+//appointmentChange
