eec4e5c380b918e5f14abc4091bbd1b2b34bda86
meh3000 <50619849+meh3000@users.noreply.github.com>
2019-07-02 12:57:13 +0200

    Update terminanzeige.cpp

--- src/terminanzeige.cpp
+++ src/terminanzeige.cpp
@@ -11,11 +11,10 @@ struct Appointment {
 	char *date[100];
 	char *time[100];
 	char *description[200];
-	int userId;
-	int appointmentId;
+	char userId;
+	char appointmentId;
 };
-
-void appointmentHTMLausgabe(const char fname, int userId){
+bool appointmentHTMLausgabe(const char fname, char *user){
 	Appointment * test1 = 0;
 	size_t num = 0;
 	test1 = (Appointment*)readStructs(&fname, &num, sizeof(Appointment));
@@ -24,8 +23,9 @@ void appointmentHTMLausgabe(const char fname, int userId){
 	char *userID;
 	userID = getValueOfKey(cookieData, (char*)"id", ';');
 
-	if ((char)*userID != userId){
-		cout << "Der User hat keine oder eine falsche id";
+	if (userID != user){
+		//Der User kann eine falschee Id
+		return 0;
 	}
 	else{
 		for (int i = 0; i <= num; i++){
@@ -38,32 +38,28 @@ void appointmentHTMLausgabe(const char fname, int userId){
 			cout << "< / tr>";
 		}
 	}
-}bool appointmentAdd(const char fname,int appointmentId, int userId){
-	Appointment * addAp = 0;
+}
+bool appointmentAdd(const char fname,char *des, char appointmentId, char *user){
+	Appointment * addAp;
+	Appointment * tmpAdd;
 	size_t num = 0;
 	
 	addAp = (Appointment*)readStructs(&fname, &num, sizeof(Appointment));
 	if (appointmentId == NULL){
-		return 0;
+		addAp = (Appointment*)malloc((num+1)* sizeof(Appointment));
+		//PRÜFUNG
 	}
 	else
-	{
+	{	
+		addAp[num].appointmentId = 0;
+		strcpy_s(addAp[num].description, des);//char,const char 
 	}
-	writeStructs(&fname, &num, sizeof(Appointment));
+	writeStructs(&fname, addAp, num + 1, sizeof(Appointment));
 
 	return 1;
 }
-void writeStructs(const char *fname, void *data, size_t num, size_t element_size);
-/*
-void dateTime(){
-//This functions returns an timestamp from a date
-//char *date          takes a date as "yyyy-mm-dd"
-//returns             date as timestamp (comparable with >, <, ==, ...)
-time_t getTimestamp(char *date);
-}*/
-void descriptionF(){
-	cout << "Test"; //aus lesen aus struct
-}
+
+//void writeStructs(const char *fname, void *data, size_t num, size_t element_size);
 
 //appointmentDelete
 
