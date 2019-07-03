3f111a244cd51e6b8fe583dfb58ddec165f22ac5
meh3000 <50619849+meh3000@users.noreply.github.com>
2019-07-03 09:17:18 +0200

    Update terminatoranzeige.cpp

--- src/terminanzeige.cpp
+++ src/terminanzeige.cpp
@@ -4,9 +4,9 @@
 #include <string.h>
 #include "fileFunctions.h"
 #include "formFunctions.h"
-
+ 
 using namespace std;
-
+ 
 struct Appointment {
 	char *date[100];
 	char *time[100];
@@ -14,53 +14,57 @@ struct Appointment {
 	char userId;
 	char appointmentId;
 };
+
 bool appointmentHTMLausgabe(const char fname, char *user){
 	Appointment * test1 = 0;
 	size_t num = 0;
 	test1 = (Appointment*)readStructs(&fname, &num, sizeof(Appointment));
 
-	char *cookieData = getenv("HTTP_COOKIE");// getenv wird schon in der Main verwendent! 
-	char *userID;
-	userID = getValueOfKey(cookieData, (char*)"id", ';');
-
-	if (userID != user){
-		//Der User kann eine falschee Id
+char savedUserId[8] = "13,";
+	char *givenUserId = "13";
+/*	char *userID;
+	userID = getValueOfKey(cookieData, (char*)"id", ';'); */
+ 
+/*	if (userID != user)*/
+if(givenUserId != savedUserId)
+{
+		//Der User hat eine falschee Id
 		return 0;
 	}
 	else{
 		for (int i = 0; i <= num; i++){
 			cout << "Content-type:text/html\r\n\r\n";
 			cout << "<tr>";
-			// Error weil beim ausgeben- > operator wird nicht erkannt
-			//cout << "<td>" << dateTime() << "< / td>";
-			//cout << "<td>" << descriptionF() << ";< / td>";
+			cout << "<td>" << test1[num].date << "< / td>";
+			cout << "<td>" << test1[num].description << ";< / td>";
 			cout << "<td><button><i class = 'fa fa - pencil'>ändern< / i>< / button><button><i class = 'fa fa - trash'>löschen< / i>< / button>< / td>";
 			cout << "< / tr>";
 		}
 	}
 }
+
 bool appointmentAdd(const char fname,char *des, char appointmentId, char *user){
 	Appointment * addAp;
 	Appointment * tmpAdd;
-	size_t num = 0;
+	size_t nr = 0;
 	
-	addAp = (Appointment*)readStructs(&fname, &num, sizeof(Appointment));
-	if (appointmentId == NULL){
+	addAp = (Appointment*)readStructs(&fname, &nr, sizeof(Appointment));
+	
+	if(appointmentId == appointmentId){
+		return false; }
+	if(appointmentId != appointmentId || appointmentId == NULL){
 		addAp = (Appointment*)malloc((num+1)* sizeof(Appointment));
-		//PRÜFUNG
-	}
-	else
-	{	
+		//PRÜFUNG muss noch rein	
 		addAp[num].appointmentId = 0;
 		strcpy_s(addAp[num].description, des);//char,const char 
 	}
 	writeStructs(&fname, addAp, num + 1, sizeof(Appointment));
-
+ 
 	return 1;
 }
-
+ 
 //void writeStructs(const char *fname, void *data, size_t num, size_t element_size);
-
+ 
 //appointmentDelete
-
+ 
 //appointmentChange
