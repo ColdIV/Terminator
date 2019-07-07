37084bf941fa3ca4aaeac6bc8d25d498a318bbc7
meh3000 <50619849+meh3000@users.noreply.github.com>
2019-07-07 12:33:59 +0200

    add appointmentFunctions.h

---
+++ src/appointmentFunctions.h
@@ -0,0 +1,19 @@
+#ifndef _appointmentFunctions_h_
+#define _appointmentFunctions_h_
+
+struct Appointment {
+	char* date[100];
+	char* time[100];
+	char* description[200];
+	int userId;
+	int appointmentId;
+};
+
+bool appointmentHTMLausgabe(const char fname, char* user);
+bool makeAppointmentID(const char fname);
+bool getDataApp(char* data); 
+bool appointmentAdd(const char fname, char* sdate, char* stime, char* sdescription);
+bool appointmentChange(const char fname, char* sdate, char* stime, char* sdescription);
+bool deleteAppoi(const char fname, char* sappointmentId);
+
+#endif
